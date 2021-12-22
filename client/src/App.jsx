568613f294470dccc4317f5df154566eb7557e92
unknown <landon3388@gmail.com>
2021-12-22 10:23:03 -0500

    red

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-    <h3>peru</h3>
+      <h4>red</h4>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <div>app</div>} />
+          <Route path="/home" element={<Home/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
